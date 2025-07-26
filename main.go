37d1d212f09c6eaecf525231c379421f88f8f7c1
karthik <karthiknadar1204@gmail.com>
2025-07-26 07:06:46 +0530

    create routes and update notes

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+
+	"restaurant_backend/database"
+
+	middleware "restaurant_backend/middleware"
+	routes "restaurant_backend/routes"
+
+	"github.com/gin-gonic/gin"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+}
